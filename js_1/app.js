55d91637183636ec1586a42ee122c4b8557527e7
Mirdcafar <rmirdzafar@gmail.com>
2024-05-29 10:31:32 +0400

    js

--- js_1/app.js
+++ js_1/app.js
@@ -3,12 +3,13 @@ let price = +prompt("Write price number")
 
 switch (ValuteName) {
     case 'azn':
-        console.log(price * 1.7);
+        var allPrice = price * 1.7
+        alert("AZN " + allPrice)
         break;
     case 'usd':
-        console.log(price / 1.7);
+        var allPrice = price / 1.7
+        alert("USD " + allPrice)
         break;
-
     default:
         break;
 }
