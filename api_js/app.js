923ed2fed02d9b3b727b3dfa197b12f24c416b8b
Mirdcafar <rmirdzafar@gmail.com>
2024-06-10 17:44:35 +0400

    js_api

--- api_js/app.js
+++ api_js/app.js
@@ -3,13 +3,14 @@ let key = '0fbcffaadcddd020d1db273e319e9c3b';
 let countryName = prompt("Pleace write country name")
 
 async function main() {
-    const url = `https://api.openweathermap.org/data/3.0/onecall?exclude=${countryName}&appid=${key}`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${countryName}&units=metric&appid=${key}`;
 
     let response = await fetch(url);
     let data = await response.json();
-    console.log(data.lon);
+    console.log(data.main.temp);
 
 }
 
 main();
 
+// $"{_apiUrl}/weather?q={cityName}&appid={_appId}&units={_units}"
